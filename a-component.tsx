1646f2ef53839361eb6cc8d055f216a0f09ab22f
citrus327 <citrus327@outlook.com>
2023-09-13 11:38:51 +0800

    ehhh

---
+++ a-component.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Button = (props) => {
+  const { className } = props
+
+  return <div {className}>Button</div>
+}
+export const Box = () => <div>Box</div>
+export const Table = () => <div>Table</div>
